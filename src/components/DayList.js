d8d5cef8e74d82ff63f9907372a80fc8f844549c
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-18 20:33:51 -0300

    incorporated the DayList component into the Application.  Imported useState in application and stored the day state in the Application component. Set default day state to Monday and made DayList component receive the alue represented by the state and also receive the function that can update the state

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,7 +4,6 @@ import DayListItem from "./DayListItem";
 function DayList(props) {
   
   const daysArray = props.days.map((day) => {
-   
     const selected = day.name === props.day ? true : false;
     
     return < DayListItem key={day.id} name={day.name} spots={day.spots} selected={selected} setDay={props.setDay}/>
