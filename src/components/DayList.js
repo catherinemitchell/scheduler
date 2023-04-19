24fc701b679d03a9825cf7cc2b0c7115843e31ef
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-19 16:26:33 -0300

    updated Application.js, DayList.js, InterviewerList.js and stories/index.js files with controlled list naming conventions value and onChange

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -4,9 +4,9 @@ import DayListItem from "./DayListItem";
 function DayList(props) {
   
   const daysArray = props.days.map((day) => {
-    const selected = day.name === props.day ? true : false;
+    const selected = day.name === props.value ? true : false;
     
-    return < DayListItem key={day.id} name={day.name} spots={day.spots} selected={selected} setDay={props.setDay}/>
+    return < DayListItem key={day.id} name={day.name} spots={day.spots} selected={selected} setDay={props.onChange}/>
   })
 
   return (
