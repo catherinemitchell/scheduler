f2473920da9a50cfeda1253b68d9abbbb425eef1
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-19 20:05:07 -0300

    used useState in Form component.  Implemented controlled components to capture a students name and update it.  Added props to the InterviewerList. Implemented onCancel and onSave props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import InterviewerList from "components/InterviewerList";
 import Button from "components/Button";
 
-export default function Form() {
+export default function Form(props) {
+  const [student, setStudent] = useState(props.student || "");
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -12,24 +15,23 @@ export default function Form() {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-          /*
-            This must be a controlled component
-            your code goes here
-          */
+            value={student}
+            onChange={(event) => setStudent(event.target.value)}
           />
         </form>
         <InterviewerList
-        /* your code goes here */
+        interviewers={props.interviewers}
+        value={interviewer}
+        onChange={setInterviewer}
+
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger >Cancel</Button>
-          <Button confirm >Save</Button>
-          {/* <Button confirm your code goes here>Save</Button> */}
+          <Button onClick={props.onCancel} danger>Cancel</Button>
+          <Button onClick={props.onSave} confirm>Save</Button>
         </section>
       </section>
     </main>
   );
 }
-
