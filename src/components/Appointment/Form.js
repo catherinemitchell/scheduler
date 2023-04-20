b264c2637ff58f3fc496f9ce981d5c420c3ee4c8
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-20 09:33:17 -0300

    updated the show appointment component to display the Show component when someone books an interview

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -18,8 +18,8 @@ export default function Form(props) {
 
   return (
     <main className="appointment__card appointment__card--create">
-      <section className="appointment__card-left">
-        <form autoComplete="off">
+      <section className="appointment__card-left" >
+        <form onSubmit={event => event.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
@@ -38,7 +38,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button onClick={cancel} danger>Cancel</Button>
-          <Button onClick={props.onSave} confirm>Save</Button>
+          <Button onClick={() => props.onSave(student, interviewer)} confirm>Save</Button>
         </section>
       </section>
     </main>
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import "./styles.scss";
+import Header from "./Header.js";
+import Empty from "./Empty.js";
+import Show from "./Show.js";
+
 
 export default function Appointment(props) {
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ? 
+      (<Show 
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+      />) : (<Empty />)}
+    </article>
   );
 }
 
