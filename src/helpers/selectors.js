1636a09a45bc61965c82067782a5fa37056a4371
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-21 20:27:37 -0300

    added tests for getInterviewersByDay as well as added interviewers array to days object.  Implemented getInterviewersByDay function to the selectors file

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,28 @@
 export function getAppointmentsForDay(state, day) {
+  //- need an empty list to begin with
+  // get the day object for this day
+  //get the day object appointments
+  //loop throught the appointments array
+  //forEach apointment id, get the appointment that matches that id
+  //add that appointment to our list 
+  //return the list
+
+//   let array = [];
+
+//   const dayObj = state.days.find(d => d.name === day)
+//   if (dayObj === 0 || dayObj === undefined) {
+//     return [];
+//   }
+
+//   for (const id of dayObj.appointments ) {
+//     const appointment = state.appointments[id]
+
+//     array.push[appointment]
+//   }
+
+  
+//   return array
+// }
   
   const hasDays = Boolean(state.days.length)
   if (!hasDays) {
@@ -40,6 +64,48 @@ export function getInterview(state, originalInterview) {
 return {...originalInterview, interviewer: interviewerObject}
 
 }
+
+export function getInterviewersForDay(state, day) {
+  //- need an empty list to begin with
+  // get the day object for this day
+  //get the day object interviewers
+  //loop throught the interviewers array
+  //forEach interviewers id, get the interviewers that matches that id
+  //add that appointment to our list 
+  //return the list
+
+//   let array = [];
+
+//   const dayObj = state.days.find(d => d.name === day)
+
+//   for (const id of dayObj.interviewers ) {
+//     const interviewer = state.interviewers[id]
+
+//     array.push[interviewer]
+//   }
+//   return array
+// }
+
+const hasInterviewers = Boolean(state.days.length)
+if (!hasInterviewers) {
+  return []
+}
+let foundDay;
+for (const dayObj of state.days) {
+  if (dayObj.name === day) {
+    foundDay = dayObj
+  }
+} 
+if (!foundDay) {
+  return []
+}
+
+const result = foundDay.appointments.map((interviewersId) => {
+ const foundInterviewer = Object.values(state.appointments).find(({ id }) => id === interviewersId)
+return foundInterviewer
+})
+return result 
+};
 
 
 
