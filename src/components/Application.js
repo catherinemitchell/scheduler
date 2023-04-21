f6738dc8004bd9bd7b741c4f5ef0d03a4a9d901f
Catherine Mitchell <nicholscat@yahoo.com>
2023-04-20 22:09:36 -0300

    cleaned up console.logs in selectors.js.  In application component combined the state for day, days, and appointments, updated the props being passed to the DayList component, added the setDay and setDays function using the spread operator and prev.

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,11 +45,18 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+  day: "Monday",
+  days: [],
+  appointments: {}
+});
+
+const setDay = day => setState(prev => ({ ...prev, day }));
+
+const setDays = days => setState(prev => ({...prev, days }));
 
   useEffect(() => {
-    const URL = `http://localhost:8001/api/days`;
+    const URL = `/api/days`;
     axios.get(URL)
     .then((res) => {setDays(res.data)})
   }, [])
@@ -65,8 +72,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -6,23 +6,17 @@ export function getAppointmentsForDay(state, day) {
   }
   let foundDay;
   for (const dayObj of state.days) {
-    // console.log(day)
     if (dayObj.name === day) {
-      console.log("Found NAME!")
       foundDay = dayObj
     }
   } 
   if (!foundDay) {
     return []
   }
-  console.log("Selected day after loop", foundDay)
-  // console.log(Object.values(state.appointments))
+
   const result = foundDay.appointments.map((appointmentId) => {
-    // console.log("Appointment ID", appointmentId)
    const foundAppointment = Object.values(state.appointments).find(({ id }) => id === appointmentId)
-    console.log("FOUND APP", foundAppointment)
   return foundAppointment
   })
-  console.log("Finished?", result)
   return result 
 }
